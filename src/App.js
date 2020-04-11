5f3c43791b8d2e35bb82739cab60d514386393e5
mathetes <mathetes@gmail.com>
2020-04-11 18:14:08 +0700

    included components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function MovieItem(props) {
-  return (
-    <div>
-      <p>{props.title}</p>
-      <p>{props.vote_average}</p>
+const movie = {
+  title: "Avengers: Infinity War",
+  vote_average: 8.5,
+  image: "https://v1.popcornnews.ru/k2/news/970/upload/Rqci0u.jpg", 
+  overview: "As the Avengers and their allies have continued to protect the world from threats"
+};
+
+function Image(props) {
+  console.log ("Image props", props);
+  return <img src={props.src} alt={props.alt} />;
+}
+
+// function MovieItem(props) {
+//   console.log ("props", props);
+//   const {data: { title, vote_average, image } } = props;
+//   return (
+//     <div>
+//       <Image src={image} alt={title} />
+//       <p>{title}</p>
+//       <p>{vote_average}</p>
+//     </div>
+//   );
+// }
+
+class MovieItem extends React.Component {
+  render() {
+    const {data: { title, vote_average, image } } = this.props;
+    console.log (this);
+    return (
+      <div>
+        <Image src={image} alt={title} />
+        <p>{title}</p>
+        <p>{vote_average}</p>
     </div>
-  );
+    );
+  }
 }
 
 function App() {
   return (
     <div>
-      <MovieItem title="Title new" vote_average={10.1} /> 
+      <MovieItem data = {movie} /> 
     </div>
   );
 }
